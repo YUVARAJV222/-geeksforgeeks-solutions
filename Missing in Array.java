826407b267b61fb1fe1fae068c44338a52e83f9b
V.YUVARAJ <v.yuvaraj.22.02.2006@gmail.com>
2026-03-08 14:23:35 +0530

    Add files via upload

--- Missing in Array.java
+++ Missing in Array.java
@@ -1,1 +1,25 @@
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        long big=0;
+        long sum =0;
+        long len =arr.length;
+        for (int i=0;i<len;i++)
+        {
+            sum=sum+arr[i];
+            if(arr[i]>big)
+            {
+                big=arr[i];
+            }
+        }
+        long a=big*(big+1);
+        a=a/2;
+        long ans=a-sum;
+        int aa=(int)ans;
+        if(ans ==0){
+            return (int)big+1;
+        }
+        return aa;
 
+    }
+}
